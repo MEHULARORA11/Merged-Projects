a830656bff729febad52ab43ecc83e432d2f9600
MEHULARORA11 <mehularora505@gmail.com>
2026-01-27 15:17:46 +0530

    merge all previous 10 mini projects

---
+++ merge.js
@@ -0,0 +1,30 @@
+buttons = document.querySelectorAll('button')
+main = document.querySelector('.main')
+
+main.addEventListener('click',(e) => {
+switch(e.target.id){
+        case "1" : window.open('../Project1/index.html','_blank')
+        break;
+        case "2" : window.open('../Project2/index.html','_blank')
+        break;
+        case "3" : window.open('../Project3/index.html','_blank')
+        break;
+        case "4" : window.open('../Project4/index.html','_blank')
+        break;
+        case "5" : window.open('../Project6/index.html','_blank')
+        break;
+        case "6" : window.open('../Project7/index.html','_blank')
+        break;
+        case "7" : window.open('../Project8/index.html','_blank')
+        break;
+        case "8" : window.open('../Project9/index.html','_blank')
+        break;
+        case "9" : window.open('../Project10/index.html','_blank')
+        break;
+        case "10" : window.open('../Project12/index.html','_blank')
+        break;
+        default:
+                  
+                   
+    }
+})
