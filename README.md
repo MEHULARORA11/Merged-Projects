df3b88e77cf9127f5eab2cb4492a0a03b38f7739
MEHULARORA11 <mehularora505@gmail.com>
2026-01-27 22:45:27 +0530

    add finishing

---
+++ README.md
@@ -0,0 +1,17 @@
+# IN This repo I have Merged My previous 10 <br> Projects have a look
+## The Projects are namely
+
+<ul>
+<li><span style="font-weight:750">Project1:</span>Color-Scheme-Switcher</li>
+<li><span style="font-weight:750">Project2:</span>BMI Calculator</li>
+<li><span style="font-weight:750">Project3:</span>Digital Clock</li>
+<li><span style="font-weight:750">Project4:</span>Random Number Game</li>
+<li><span style="font-weight:750">Project5:</span>Magic Keys</li>
+<li><span style="font-weight:750">Project6:</span>Colour Changing Game</li>
+<li><span style="font-weight:750">Project7:</span>Currency Converter</li>
+<li><span style="font-weight:750">Project8:</span>Tic Tac Toe</li>
+<li><span style="font-weight:750">Project9:</span>Rock • Paper • Scissors</li>
+<li><span style="font-weight:750">Project10:</span>Todo App</li>
+</ul>
+
+Live Demo [click here](https://projects-one-orcin.vercel.app/)
--- merge.js
+++ merge.js
@@ -3,25 +3,25 @@ main = document.querySelector('.main')
 
 main.addEventListener('click',(e) => {
 switch(e.target.id){
-        case "1" : window.open('../Project1/index.html','_blank')
+        case "1" : window.open('https://color-scheme-switcher-nine.vercel.app/','_blank')
         break;
-        case "2" : window.open('../Project2/index.html','_blank')
+        case "2" : window.open('https://bmi-calculator-ivory-rho.vercel.app/','_blank')
         break;
-        case "3" : window.open('../Project3/index.html','_blank')
+        case "3" : window.open('https://digital-clock-brown-omega.vercel.app/','_blank')
         break;
-        case "4" : window.open('../Project4/index.html','_blank')
+        case "4" : window.open('https://random-number-game-ten.vercel.app/','_blank')
         break;
-        case "5" : window.open('../Project6/index.html','_blank')
+        case "5" : window.open('https://github.com/MEHULARORA11/Magic-Keys.git','_blank')
         break;
-        case "6" : window.open('../Project7/index.html','_blank')
+        case "6" : window.open('https://github.com/MEHULARORA11/Colour_Changing_Game.git','_blank')
         break;
-        case "7" : window.open('../Project8/index.html','_blank')
+        case "7" : window.open('https://github.com/MEHULARORA11/Currency-Converter.git','_blank')
         break;
-        case "8" : window.open('../Project9/index.html','_blank')
+        case "8" : window.open('https://tic-tac-toe-game-nine-puce.vercel.app/','_blank')
         break;
-        case "9" : window.open('../Project10/index.html','_blank')
+        case "9" : window.open('https://rock-paper-scissor-game-taupe-tau.vercel.app/','_blank')
         break;
-        case "10" : window.open('../Project12/index.html','_blank')
+        case "10" : window.open('https://my-todo-app-five-ecru.vercel.app/','_blank')
         break;
         default:
                   
